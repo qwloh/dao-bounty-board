2b56061fbc957c2276fcbb3ebdc1a4a72ed990d6
Qiao Wen <loh.qwen@gmail.com>
2022-09-06 04:42:14 +0800

    App code: Added default bounty board configs to useProposeInitBountyBoard

--- app/hooks/bounty-board/useProposeInitBountyBoard.ts
+++ app/hooks/bounty-board/useProposeInitBountyBoard.ts
@@ -1,17 +1,18 @@
 import { PublicKey } from "@solana/web3.js";
 import { useMutation } from "@tanstack/react-query";
+import { BN } from "@project-serum/anchor";
 import { proposeInitBountyBoard } from "../../api";
+import { DUMMY_MINT_PK } from "../../api/constants";
 import { InitialContributorWithRole } from "../../api/utils";
 import { BountyBoardConfig } from "../../model/bounty-board.model";
+import { CallbacksForUI } from "../../model/util.model";
 import { useRealm } from "../realm/useRealm";
 import { UserProposalEntity } from "../realm/useUserProposalEntitiesInRealm";
 import { useAnchorContext } from "../useAnchorContext";
 
-export const DEFAULT_CONFIG = {};
-
 export interface ProposeInitBountyBoardArgs {
   userProposalEntity: UserProposalEntity;
-  boardConfig: BountyBoardConfig;
+  boardConfig: Omit<BountyBoardConfig, "lastRevised">;
   firstVaultMint: PublicKey;
   amountToFundBountyBoardVault: number;
   initialContributorsWithRole: InitialContributorWithRole[];
@@ -19,7 +20,8 @@ export interface ProposeInitBountyBoardArgs {
 
 export const useProposeInitBountyBoard = (
   // can be symbol or address
-  realm: string
+  realm: string,
+  callbacks: CallbacksForUI = { onSuccess: undefined, onError: undefined }
 ) => {
   const { program } = useAnchorContext();
   const { data: realmAccount } = useRealm(realm);
@@ -40,6 +42,100 @@ export const useProposeInitBountyBoard = (
         firstVaultMint,
         amountToFundBountyBoardVault,
         initialContributorsWithRole
-      )
+      ),
+    {
+      onSuccess: (data, variables, context) => {
+        if (callbacks?.onSuccess) {
+          callbacks.onSuccess(data, variables, context);
+        }
+      },
+      onError: (err, variables, context) => {
+        console.error(err);
+        if (callbacks?.onError) {
+          callbacks?.onError(err, variables, context);
+        }
+      },
+    }
   );
+};
+
+// --- default values
+
+export const getTiersInVec = (PAYOUT_MINT: PublicKey) => [
+  {
+    tierName: "Entry",
+    difficultyLevel: "First contribution",
+    minRequiredReputation: 0,
+    minRequiredSkillsPt: new BN(0),
+    reputationReward: 10,
+    skillsPtReward: new BN(10),
+    payoutReward: new BN(50),
+    payoutMint: PAYOUT_MINT,
+    taskSubmissionWindow: 7 * 24 * 3600, // 7 days
+    submissionReviewWindow: 3 * 24 * 3600, // 3 days
+    addressChangeReqWindow: 3 * 24 * 3600, // 3 days
+  },
+  {
+    tierName: "A",
+    difficultyLevel: "Easy",
+    minRequiredReputation: 50,
+    minRequiredSkillsPt: new BN(50),
+    reputationReward: 20,
+    skillsPtReward: new BN(20),
+    payoutReward: new BN(200),
+    payoutMint: PAYOUT_MINT,
+    taskSubmissionWindow: 14 * 24 * 3600, // 14 days
+    submissionReviewWindow: 7 * 24 * 3600, // 7 days
+    addressChangeReqWindow: 7 * 24 * 3600, // 7 days
+  },
+  {
+    tierName: "AA",
+    difficultyLevel: "Moderate",
+    minRequiredReputation: 100,
+    minRequiredSkillsPt: new BN(100),
+    reputationReward: 50,
+    skillsPtReward: new BN(50),
+    payoutReward: new BN(500),
+    payoutMint: PAYOUT_MINT,
+    taskSubmissionWindow: 30 * 24 * 3600, // 30 days
+    submissionReviewWindow: 7 * 24 * 3600, // 7 days
+    addressChangeReqWindow: 7 * 24 * 3600, // 7 days
+  },
+  {
+    tierName: "S",
+    difficultyLevel: "Complex",
+    minRequiredReputation: 500,
+    minRequiredSkillsPt: new BN(500),
+    reputationReward: 100,
+    skillsPtReward: new BN(100),
+    payoutReward: new BN(2000),
+    payoutMint: PAYOUT_MINT,
+    taskSubmissionWindow: 60 * 24 * 3600, // 60 days
+    submissionReviewWindow: 14 * 24 * 3600, // 14 days
+    addressChangeReqWindow: 14 * 24 * 3600, // 14 days
+  },
+];
+
+enum Permission {
+  CreateBounty = "createBounty",
+  UpdateBounty = "updateBounty",
+  DeleteBounty = "deleteBounty",
+  AssignBounty = "assignBounty",
+  RequestChangeToSubmission = "requestChangeToSubmission",
+  AcceptSubmission = "acceptSubmission",
+  RejectSubmission = "rejectSubmission",
+}
+
+export const getRolesInVec = () => [
+  {
+    roleName: "Core",
+    permissions: Object.values(Permission).map((p) => ({ [p]: {} })),
+    default: false,
+  },
+  { roleName: "Contributor", permissions: [], default: true },
+];
+
+export const DEFAULT_CONFIG = {
+  roles: getRolesInVec(),
+  tiers: getTiersInVec(new PublicKey(DUMMY_MINT_PK.USDC)),
 };
